74318ed1c9a3ba1ca6f36c11db32373a46899194
shalldie <hi.xieshuang@gmail.com>
2019-06-20 15:48:09 +0800

    添加单测 & release v0.0.3 :tada::tada: :cat:

--- test/h.test.ts
+++ test/h.test.ts
@@ -22,4 +22,30 @@ describe('function h', () => {
         expect(vnode.children[0].data.attrs.name).toBe('tom');
     });
 
+    test('h(type: string, data?: IVNodeData, text?: string)', () => {
+        const vnode = h('div.hello', {
+            attrs: {
+                'data-name': 'tom'
+            }
+        }, 'world');
+
+        expect(vnode.type).toBe('div');
+        expect(vnode.data.attrs.class).toBe('hello');
+        expect(vnode.data.attrs['data-name']).toBe('tom');
+        expect(vnode.children).toHaveLength(1);
+        expect(vnode.children[0].text).toBe('world');
+    });
+
+    test('h(type: string, data?: IVNodeData, children?: VNode[])', () => {
+        const vnode = h('div.hello', {}, [
+            h('span', 'world')
+        ]);
+
+        expect(vnode.type).toBe('div');
+        expect(vnode.data.attrs.class).toBe('hello');
+        expect(vnode.children).toHaveLength(1);
+        expect(vnode.children[0].type).toBe('span');
+        expect(vnode.children[0].children[0].text).toBe('world');
+    });
+
 });
